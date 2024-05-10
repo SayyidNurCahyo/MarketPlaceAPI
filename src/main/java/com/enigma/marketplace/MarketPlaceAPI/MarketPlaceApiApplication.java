e1b5ea242944a11cb03fe95a5ab96615fd16e070
Sayyid Nur Cahyo Abdul Jalil <sayyidnurc123@gmail.com>
2024-05-10 08:31:19 +0700

    initial commit

---
+++ src/main/java/com/enigma/marketplace/MarketPlaceAPI/MarketPlaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.marketplace.MarketPlaceAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPlaceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/marketplace/MarketPlaceAPI/MarketPlaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.marketplace.MarketPlaceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
